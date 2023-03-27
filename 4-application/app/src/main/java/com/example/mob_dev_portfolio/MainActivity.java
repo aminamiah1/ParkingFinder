6125b59bd3870fb55c9da93451c8d6309806b3c6
Amina Miah <miaha9@cardiff.ac.uk>
2023-03-27 13:14:26 +0100

    FrontEnd Design

--- 4-application/app/src/main/java/com/example/mob_dev_portfolio/MainActivity.java
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 
 import com.example.mob_dev_portfolio.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ActivityMainBinding binding;
 
@@ -17,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.search_button:
+                //TODO: implement search functionality
+                break;
+        }
     }
 }
---
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/SearchResultsAdapter.java
@@ -0,0 +1,4 @@
+package com.example.mob_dev_portfolio;
+
+public class SearchResultsAdapter {
+}
